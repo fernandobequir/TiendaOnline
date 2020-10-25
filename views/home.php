5bced3b7ce00e491ce11f073662f75e9693ecca8
Fernando Bequir <fernandobequir@hotmail.com>
2020-10-25 17:23:21 -0300

    ruta de item de producto

--- views/home.php
+++ views/home.php
@@ -7,7 +7,7 @@
 		<div class="grid-productos">
 			<!-- Producto #1 -->
 			<div class="item-grid-productos">
-				<a href="?menu=producto">
+				<a href="?menu=producto&item=#">
                     <img class="img-grid-productos" src="img/productos/P001.jpg" alt=" " />
 				    <div class="detalle-grid-productos">
                         <span>$ 300</span>
